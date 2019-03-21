067905fc044d5f81ef29a2120d9da55f013d0dc0
ztex <ztex030640417@gmail.com>
2019-03-21 11:45:24 +0800

    Feat Set up attack flow

--- DAA.py
+++ DAA.py
@@ -1,5 +1,32 @@
 from scapy.all import *
 
-# construct IP packet
+# Construct IP packet
 ip = IP()
 ip.show()
+
+# Construct UDP packet
+udp = UDP()
+u.display()
+
+# Construct DNS packet
+dns = DNS()
+dns.display()
+
+# Construct DNS Question Record
+q = DNSQR()
+q.display()
+
+# Set DNS Question Record in DNS packet
+dns.qd = q
+
+# Concencate
+r = (ip/udp/dns)
+r.display()
+sr1(r)
+
+# Set up r
+r.src = ''
+r = (ip/udp/dns)
+r.display()
+send(r)
+
